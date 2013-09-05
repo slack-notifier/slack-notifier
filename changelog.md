e9f90629e26d2da5c8e951aa5dcb70821d79544e
Steven Sloan <stevenosloan@gmail.com>
2013-09-05 17:44:22 -0400

    bump version, add changelog

---
+++ changelog.md
@@ -0,0 +1,5 @@
+# 0.0.2
+- fix a fat finger if a default channel is set
+
+# 0.0.1
+- initial release
--- lib/slack-notifier/version.rb
+++ lib/slack-notifier/version.rb
@@ -1,5 +1,5 @@
 module Slack
   class Notifier
-    VERSION = "0.0.1"
+    VERSION = "0.0.2"
   end
 end
