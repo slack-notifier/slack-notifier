fc24aa4ca00b98f9d59125d1a65baff6a30d20d8
Steven Sloan <stevenosloan@gmail.com>
2014-02-28 01:46:08 -0500

    add spec for the notifier

--- spec/lib/slack-notifier/link_formatter_spec.rb
+++ spec/lib/slack-notifier/link_formatter_spec.rb
@@ -2,19 +2,23 @@ require 'spec_helper'
 
 describe Slack::Notifier::LinkFormatter do
 
-  it "formats html links" do
-    formatted = described_class.format("Hello World, enjoy <a href='http://example.com'>this</a>.")
-    expect( formatted ).to include("<http://example.com|this>")
-  end
+  describe "::format" do
 
-  it "formats markdown links" do
-    formatted = described_class.format("Hello World, enjoy [this](http://example.com).")
-    expect( formatted ).to include("<http://example.com|this>")
-  end
+    it "formats html links" do
+      formatted = described_class.format("Hello World, enjoy <a href='http://example.com'>this</a>.")
+      expect( formatted ).to include("<http://example.com|this>")
+    end
 
-  it "formats markdown links with no title" do
-    formatted = described_class.format("Hello World, enjoy [](http://example.com).")
-    expect( formatted ).to include("<http://example.com>")
+    it "formats markdown links" do
+      formatted = described_class.format("Hello World, enjoy [this](http://example.com).")
+      expect( formatted ).to include("<http://example.com|this>")
+    end
+
+    it "formats markdown links with no title" do
+      formatted = described_class.format("Hello World, enjoy [](http://example.com).")
+      expect( formatted ).to include("<http://example.com>")
+    end
+
   end
 
 end
--- spec/lib/slack-notifier_spec.rb
+++ spec/lib/slack-notifier_spec.rb
@@ -2,4 +2,74 @@ require 'spec_helper'
 
 describe Slack::Notifier do
 
+  describe "#initialize" do
+    it "sets the given team" do
+      subject = described_class.new 'team', 'token'
+      expect( subject.team ).to eq 'team'
+    end
+
+    it "sets the given token" do
+      subject = described_class.new 'team', 'token'
+      expect( subject.token ).to eq 'token'
+    end
+  end
+
+  describe "#ping" do
+    it "passes the message through LinkFormatter" do
+      allow( HTTParty ).to receive(:post)
+      expect( Slack::Notifier::LinkFormatter ).to receive(:format)
+                                              .with("the message")
+
+      described_class.new('team','token').ping "the message", channel: 'foo'
+    end
+
+    it "requires a channel to be set" do
+      allow( HTTParty ).to receive(:post)
+
+      expect{
+        described_class.new('team','token').ping "the message"
+      }.to raise_error
+    end
+
+    context "with a default channel set" do
+
+      before :each do
+        allow( HTTParty ).to receive(:post)
+        @subject = described_class.new('team','token')
+        @subject.channel = 'default'
+      end
+
+      it "does not require a channel to ping" do
+        expect{
+          @subject.ping "the message"
+        }.not_to raise_error
+      end
+
+      it "uses default channel" do
+        expect( HTTParty ).to receive(:post)
+                          .with "https://team.slack.com/services/hooks/incoming-webhook?token=token",
+                                body: 'payload={"text":"the message","channel":"default"}'
+
+        @subject.ping "the message"
+      end
+
+      it "allows override channel to be set" do
+        expect( HTTParty ).to receive(:post)
+                          .with "https://team.slack.com/services/hooks/incoming-webhook?token=token",
+                                body: 'payload={"text":"the message","channel":"new"}'
+
+        @subject.ping "the message", channel: "new"
+      end
+
+    end
+
+    it "posts with the correct endpoint & data" do
+        expect( HTTParty ).to receive(:post)
+                          .with "https://team.slack.com/services/hooks/incoming-webhook?token=token",
+                                body: 'payload={"text":"the message","channel":"channel"}'
+
+        described_class.new("team","token").ping "the message", channel: "channel"
+    end
+  end
+
 end
