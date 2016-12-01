9c0b798701984c03cd3cef7ea8dc2b3d597c2355
Steven Sloan <stevenosloan@gmail.com>
2016-12-01 01:12:40 -0500

    feat(config): set endpoint by parsing given url to URI

--- lib/slack-notifier/config.rb
+++ lib/slack-notifier/config.rb
@@ -1,6 +1,12 @@
 module Slack
   class Notifier
     class Config
+      attr_reader :endpoint
+
+      def endpoint= url
+        @endpoint = URI.parse url
+      end
+
       %w[client defaults middleware].each do |m|
         define_method m do |*args|
           return instance_variable_get("@_#{m}") if args.empty?
--- spec/lib/slack-notifier/config_spec.rb
+++ spec/lib/slack-notifier/config_spec.rb
@@ -1,4 +1,14 @@
 RSpec.describe Slack::Notifier::Config do
+  describe "#endpoint=" do
+    it "sets #endpoint with parsed URI" do
+      subject = described_class.new
+      subject.endpoint = "http://example.com"
+
+      expect( subject.endpoint ).to be_a URI
+      expect( subject.endpoint.host ).to eq "example.com"
+    end
+  end
+
   %w[client defaults middleware].each do |config_method|
     it "sets the value(s) when called with arguments, returns when called with none" do
       args = %w[one two]
