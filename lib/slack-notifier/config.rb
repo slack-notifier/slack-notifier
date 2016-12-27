ebc9c36064ed48fc46e20dbd290873471c09e276
Steven Sloan <stevenosloan@gmail.com>
2016-12-26 23:49:19 -0500

    feat(config): implement & spec http_client set & receiver

--- lib/slack-notifier/config.rb
+++ lib/slack-notifier/config.rb
@@ -3,8 +3,22 @@ module Slack
     class Config
       attr_reader :endpoint
 
+      def initialize
+        @http_client = Util::HTTPClient
+      end
+
       def endpoint= url
         @endpoint = URI.parse url
+      end
+
+      def http_client client_class=nil
+        return @http_client if client_class.nil?
+
+        if client_class.respond_to?(:post)
+          @http_client = client_class
+        else
+          raise ArgumentError, "the http client must respond to ::post"
+        end
       end
 
       %w[client defaults middleware].each do |m|
--- spec/lib/slack-notifier/config_spec.rb
+++ spec/lib/slack-notifier/config_spec.rb
@@ -1,11 +1,37 @@
+# frozen_string_literal: true
+
 RSpec.describe Slack::Notifier::Config do
   describe "#endpoint=" do
     it "sets #endpoint with parsed URI" do
       subject = described_class.new
       subject.endpoint = "http://example.com"
 
-      expect( subject.endpoint ).to be_a URI
-      expect( subject.endpoint.host ).to eq "example.com"
+      expect(subject.endpoint).to be_a URI
+      expect(subject.endpoint.host).to eq "example.com"
+    end
+  end
+
+  describe "#http_client" do
+    it "is Util::HTTPClient if not set" do
+      subject = described_class.new
+      expect(subject.http_client).to eq Slack::Notifier::Util::HTTPClient
+    end
+
+    it "sets a new client class if given one" do
+      new_client = class_double("Slack::Notifier::Util::HTTPClient", post: nil)
+      subject    = described_class.new
+      subject.http_client new_client
+      expect(subject.http_client).to eq new_client
+    end
+
+    it "raises an ArgumentError if given class does not respond to ::post" do
+      subject = described_class.new
+      expect do
+        subject.http_client :nope
+      end.to raise_error ArgumentError
+    end
+  end
+
     end
   end
 
