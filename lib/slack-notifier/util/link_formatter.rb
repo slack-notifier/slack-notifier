45f2aa428060994defab8564bb74be25d5ade9e5
Pedro Furtado <pedro.felipe.azevedo.furtado@gmail.com>
2021-10-11 18:33:14 -0300

    Update link_formatter.rb

--- lib/slack-notifier/util/link_formatter.rb
+++ lib/slack-notifier/util/link_formatter.rb
@@ -40,16 +40,14 @@ module Slack
           @orig    = string.respond_to?(:scrub) ? string.scrub : string
         end
 
-        # rubocop:disable Lint/RescueWithoutErrorClass
         def formatted
           return @orig unless @orig.respond_to?(:gsub)
 
           sub_markdown_links(sub_html_links(@orig))
-        rescue => e
+        rescue StandardError => e
           raise e unless RUBY_VERSION < "2.1" && e.message.include?("invalid byte sequence")
           raise e, "#{e.message}. Consider including the 'string-scrub' gem to strip invalid characters"
         end
-        # rubocop:enable Lint/RescueWithoutErrorClass
 
         private
 
